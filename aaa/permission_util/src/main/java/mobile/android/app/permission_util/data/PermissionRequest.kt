81cd83b92a94291bf2e193437302e136e5bb5fe1
3i-Piobae <129713923+3i-Piobae@users.noreply.github.com>
2023-07-13 16:40:47 +0900

    ?

--- aaa/permission_util/src/main/java/mobile/android/app/permission_util/data/PermissionRequest.kt
+++
@@ -1,7 +0,0 @@
-package mobile.android.app.permission_util.data
-
-data class PermissionRequest constructor(
-    val requestPermissions: List<String>
-) {
-    constructor(vararg item: String) : this(listOf(*item))
-}
--- aaa/permission_util/src/main/java/mobile/android/app/permission_util/handler/PermissionHandler.kt
+++
@@ -1,9 +0,0 @@
-package mobile.android.app.permission_util.handler
-
-abstract class PermissionHandler {
-
-    internal val callback: (PermissionCallbackState) -> Unit = { onCallback(it) }
-
-    abstract fun onCallback(callbackState: PermissionCallbackState)
-
-}
